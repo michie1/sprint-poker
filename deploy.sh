ab38f56e5bde58fdfad2062197ad35883d0bf79d
Michiel <git@fastfox.nl>
2018-06-16 09:22:00 +0200

    Also deploy dist/index.js

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 elm-make src/Main.elm --output dist/sprintpoker.js &&
 scp dist/sprintpoker.js woot:/home/michiel/woot/web/www/sp.msvos.nl/ &&
+scp dist/index.js woot:/home/michiel/woot/web/www/sp.msvos.nl/ &&
 rm dist/sprintpoker.js
